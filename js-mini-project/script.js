2848ee808be716fd4d9ebcc3cdb7a8a8cbe9c972
SANOBAR T. <sanobartuhtasinova40@gmail.com>
2026-01-28 17:13:24 +0100

    Update script.js

--- js-mini-project/script.js
+++ js-mini-project/script.js
@@ -1,23 +1,32 @@
-const input = document.getElementById("todoInput");
-const button = document.getElementById("addBtn");
-const list = document.getElementById("todoList");
-const counter = document.getElementById("counter");
+const form = document.querySelector("#todoForm");
+const input = document.querySelector("#todoInput");
+const list = document.querySelector("#todoList");
+const counter = document.querySelector("#counter");
 
-let count = 0;
+let todos = [];
 
-button.addEventListener("click", () => {
+function render() {
+  list.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.textContent = todo;
+    list.appendChild(li);
+  });
+
+  counter.textContent = `${todos.length} задач`;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
   const text = input.value.trim();
+  if (!text) return;
 
-  if (text === "") {
-    return;
-  }
-
-  const li = document.createElement("li");
-  li.textContent = text;
-  list.appendChild(li);
-
-  count++;
-  counter.textContent = ${count} задач;
-
+  todos.unshift(text);
   input.value = "";
+
+  render();
 });
+
+render();
